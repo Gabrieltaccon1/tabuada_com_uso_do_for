d4a84c7e4377cf6d09b99a1e5e56a788528457d4
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 16:03:47 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 0, resultado;
+
+
+            string nome;
+
+            Console.WriteLine("DIGITE O VALOR QUE DESEJA FAZER A TABUADA");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = i * x;
+                Console.WriteLine(i + " x " + x + " = " + resultado);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
